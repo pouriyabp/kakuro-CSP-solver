5434b5cf3881aebe16f269d81b98e08ae1ac2df9
pouriya <pouriya1379@gmail.com>
2021-04-16 23:36:45 +0430

    add name to nodes.

--- Node.py
+++ Node.py
@@ -7,6 +7,15 @@ class Node:
         self.domain = []
         self.rowC = None
         self.colC = None
+        self.name = None
+
+    def set_name(self):
+        if self.type == "value":
+            self.name = 'X' + str(self.rowIndex) + str(self.colIndex)
+        elif self.type == "constraint":
+            self.name = 'C' + str(self.rowIndex) + str(self.colIndex)
+        else:
+            self.name = "B"
 
     def set_value(self, value):
         if self.type == "value":
@@ -31,5 +40,7 @@ class Node:
             self.domain = array_of_domain
         else:
             return
+
     def __repr__(self):
-       return f'({self.type}-{self.value}-{self.rowC}-{self.colC})'
+        # return f'({self.type}-{self.value}-{self.rowC}-{self.colC})'
+        return f"{self.name}"
