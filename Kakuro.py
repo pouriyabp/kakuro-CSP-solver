ef8120e940578b73126a78c3b3356f76f494e131
pouriya <pouriya1379@gmail.com>
2021-04-16 23:35:59 +0430

    add print board function.

--- Kakuro.py
+++ Kakuro.py
@@ -6,3 +6,15 @@ class Kakuro:
 
     def add_nodes_to_board(self, array_of_nodes):
         self.board.append(array_of_nodes)
+
+    def print_board(self):
+        text = ""
+        for i in range(0, self.row):
+            for j in range(0, self.col):
+                # because B nodes has no number in their name
+                if str(self.board[i][j]) == 'B':
+                    text += str(self.board[i][j]) + " " * 3
+                else:
+                    text += str(self.board[i][j]) + " "
+            text += '\n'
+        return text
