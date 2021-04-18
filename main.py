f759e0297024dbc6ae860bf8f5bb33fb6f986a88
pouriyabp <pouriya1379@gmail.com>
2021-04-18 21:01:49 +0430

    replace set_domain with calculate_domain.

--- main.py
+++ main.py
@@ -53,5 +53,5 @@ if __name__ == '__main__':
 
         print(game.print_board())
         game.find_neighbors()
-        game.set_domain_to_each_node()
+        game.calculate_domain()
         game.solve()
